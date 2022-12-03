8831d2e018b7815a7dc5098ecb44a3e4b4ae8841
datsfilipe <datsfilipe.dev@gmail.com>
2022-12-03 10:15:17 -0300

    feat(app): add app definition file

---
+++ src/app.ts
@@ -0,0 +1,20 @@
+import Fastify from 'fastify'
+import { routes } from './routes'
+
+const app = Fastify({
+  logger: true
+})
+
+app.get('/', (request, response) => {
+  response.send('API is online, baby, less gooo!')
+})
+
+app.register((appReference, _, done) => {
+  routes.forEach(route => {
+    appReference.route(route)
+  })
+
+  done()
+}, { prefix: '/v1' })
+
+export { app }
